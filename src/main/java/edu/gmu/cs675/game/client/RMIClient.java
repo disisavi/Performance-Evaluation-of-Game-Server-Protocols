58b7cee628c1d18f324192e3fd8a1d6af81433c5
Abhijeet Mishra <dsisavi@gmail.com>
2019-03-04 19:38:45 -0500

    RMI decoupled

---
+++ src/main/java/edu/gmu/cs675/game/client/RMIClient.java
@@ -0,0 +1,68 @@
+package edu.gmu.cs675.game.client;
+
+
+import edu.gmu.cs675.game.remoteInterface.ClientInterface;
+import edu.gmu.cs675.game.remoteInterface.GameInterface;
+
+import java.awt.*;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.ServerNotActiveException;
+import java.util.Scanner;
+
+
+public class RMIClient implements ClientInterface {
+    GameInterface game;
+
+    RMIClient() throws RemoteException, NotBoundException {
+        this.game = this.getGameStub();
+    }
+
+    public GameInterface getGameStub() throws RemoteException, NotBoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Game server ip ");
+        String host = scanner.next();
+
+        Registry gameRegistry = LocateRegistry.getRegistry(host, 1024);
+        GameInterface gameStub = (GameInterface) gameRegistry.lookup("game");
+
+        return gameStub;
+    }
+
+    @Override
+    public void registerPlayer(String playerName) throws Exception {
+        this.game.registerPlayer(playerName);
+    }
+
+    @Override
+    public String getPlayerWon() throws RemoteException {
+        return game.getPlayerWon();
+    }
+
+    @Override
+    public int move(String direction) throws Exception {
+        return this.game.move(direction);
+    }
+
+    @Override
+    public Point getCurrentPosition() throws RemoteException {
+        return this.game.getCurrentPosition();
+    }
+
+    @Override
+    public String getStats() throws RemoteException {
+        return this.game.getStats();
+    }
+
+    @Override
+    public String getAllPlayerNames() throws RemoteException {
+        return this.game.getAllPlayerNames();
+    }
+
+    @Override
+    public boolean deRegisterPLayer() throws RemoteException, ServerNotActiveException {
+        return this.game.deRegisterPLayer();
+    }
+}
---
+++ src/main/java/edu/gmu/cs675/game/remoteInterface/ClientInterface.java
@@ -0,0 +1,24 @@
+package edu.gmu.cs675.game.remoteInterface;
+
+import java.awt.*;
+import java.rmi.RemoteException;
+import java.rmi.server.ServerNotActiveException;
+
+public interface ClientInterface {
+
+    void registerPlayer(String playerName) throws Exception;
+
+    String getPlayerWon() throws RemoteException;
+
+    int move(String direction) throws Exception;
+
+    Point getCurrentPosition() throws RemoteException;
+
+    String getStats() throws RemoteException;
+
+    String getAllPlayerNames() throws RemoteException;
+
+    boolean deRegisterPLayer() throws RemoteException, ServerNotActiveException;
+
+
+}
